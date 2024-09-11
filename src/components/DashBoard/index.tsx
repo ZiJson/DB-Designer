3b04284eec9c2daf3f267b4c0b77f87837bf4479
ZiJson <jasonnosajya53@gmail.com>
2024-09-11 12:45:22 +0800

    lint

--- src/components/DashBoard/index.tsx
+++ src/components/DashBoard/index.tsx
@@ -11,7 +11,7 @@ const DashBoard = () => {
   const setActiveTableId = useWorkspaceStore((state) => state.setActiveTableId);
   return (
     <div
-      className={`fixed left-0 top-[50%] h-[calc(100vh-64px)] w-96 translate-y-[-50%] rounded-r-lg bg-gray-50/80 shadow-xl ${isDashboardOpen ? "w-96" : "w-20"} p-4 transition-all duration-300 ease-in-out`}
+      className={`fixed left-0 top-[50%] h-[calc(100vh-64px)] translate-y-[-50%] rounded-r-lg bg-gray-50/80 shadow-xl ${isDashboardOpen ? "w-96" : "w-20"} p-4 transition-all duration-300 ease-in-out`}
       onClick={() => setActiveTableId(null)}
     >
       <Content />
