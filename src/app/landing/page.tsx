08ddefbac1082b6d57be480717ea0584cee4ecca
ZiJson <jasonnosajya53@gmail.com>
2024-10-18 21:00:45 +0800

    landing page

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,17 @@
+const Page = () => {
+  return (
+    <div className="bg-background">
+      <div className="mx-auto max-w-screen-lg text-center text-4xl font-bold text-primary md:text-6xl">
+        From Complex to Simple:
+        <br />
+        Build
+        <span className="bg-gradient-to-r from-[#f3a55d] to-[#c4529e] bg-clip-text px-2 text-transparent">
+          Prisma
+        </span>
+        Schemas Visually
+      </div>
+    </div>
+  );
+};
+
+export default Page;
